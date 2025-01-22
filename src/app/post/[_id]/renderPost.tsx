99f64df916080c2865da6dca5dd657d3db3804e2
Gadi Cohen <dragon@wastelands.net>
2025-01-22 11:05:12 +0200

    feat(posts): syntax highlighting in code blocks

--- src/app/post/[_id]/renderPost.tsx
+++ src/app/post/[_id]/renderPost.tsx
@@ -1,11 +1,36 @@
 import { Chip, Container, Typography } from "@mui/material";
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+// import { vscDarkPlus as style } from "react-syntax-highlighter/dist/esm/styles/prism";
+import style from "./vcsDarkPlusMod";
+
 import { formatDate } from "@/lib/format";
 import { Post } from "@/schemas";
 import Copyright from "@/copyright";
 import { PostCategories } from "@/app/categories";
 import PostViews from "@/lib/PostViews";
+
+const remarkPlugins = [remarkGfm];
+const components = {
+  code(props) {
+    const { children, className, node, ...rest } = props;
+    const match = /language-(\w+)/.exec(className || "");
+    return match ? (
+      <SyntaxHighlighter
+        {...rest}
+        PreTag="div"
+        children={String(children).replace(/\n$/, "")}
+        language={match[1]}
+        style={style}
+      />
+    ) : (
+      <code {...rest} className={className}>
+        {children}
+      </code>
+    );
+  },
+};
 
 export default function RenderPost({
   post,
@@ -29,7 +54,9 @@ export default function RenderPost({
         ))}
       </span>
       <div style={{ textAlign: "justify" }}>
-        <Markdown remarkPlugins={[remarkGfm]}>{post.src}</Markdown>
+        <Markdown remarkPlugins={remarkPlugins} components={components}>
+          {post.src}
+        </Markdown>
       </div>
       {!preview && <PostViews _id={post._id.toString()} />}
       <Copyright post={post} />
