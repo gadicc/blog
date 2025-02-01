ac4be9c7e8c45cf8be6a796fa34c6c5c2c442cb0
Gadi Cohen <dragon@wastelands.net>
2025-02-01 09:51:13 +0200

    chore(mongoRelay): move from pages to app router

--- src/app/api/mongoRelay/route.ts
+++ src/app/api/mongoRelay/route.ts
@@ -1,8 +1,12 @@
 import { MongoClient } from "mongodb";
-import makeExpressRelay from "mongodb-rest-relay/lib/express";
+import makeRelay from "mongodb-rest-relay/lib/server/nextServerlessApp";
 
 const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1";
 const client = new MongoClient(MONGO_URL);
 
 // ts fails sometimes here (but not always???) because of version mismatch
-export default makeExpressRelay((await client.connect()).db("blog"));
+// @ts-expect-error: ok
+const relay = makeRelay((await client.connect()).db("blog"));
+
+export const GET = relay;
+export const POST = relay;
