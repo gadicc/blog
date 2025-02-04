f0d40457693d1a07614b12a424a2ad0073e35d2d
Gadi Cohen <dragon@wastelands.net>
2025-02-04 08:56:31 +0000

    fix(mongoRelay): comment out @ts-expect-error

--- src/app/api/mongoRelay/route.ts
+++ src/app/api/mongoRelay/route.ts
@@ -5,7 +5,7 @@ const MONGO_URL = process.env.MONGO_URL || "mongodb://127.0.0.1";
 const client = new MongoClient(MONGO_URL);
 
 // ts fails sometimes here (but not always???) because of version mismatch
-// @ts-expect-error: ok
+// @ ts-expect-error: ok
 const relay = makeRelay((await client.connect()).db("blog"));
 
 export const GET = relay;
