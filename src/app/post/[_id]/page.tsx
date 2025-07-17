6afb161e9683a4cd0c10843db83a43f441525ab4
Gadi Cohen <dragon@wastelands.net>
2025-07-17 16:56:41 +0100

    fix(post): opengraph/twitter; console.log, why wrong metadata

--- src/app/post/[_id]/page.tsx
+++ src/app/post/[_id]/page.tsx
@@ -38,6 +38,7 @@ export async function generateMetadata(
   if (!post) throw new Error("could not find post with _id: " + _id);
 
   const parent = await _parent;
+  console.log("generateMetadata", post);
 
   // optionally access and extend (rather than replace) parent metadata
   // const previousImages = (await parent).openGraph?.images || [];
@@ -46,7 +47,15 @@ export async function generateMetadata(
     title: post.title,
     description: post.description, //  || parent.description,
     openGraph: {
+      type: "article",
+      title: post.title,
+      description: post.description,
+      siteName: parent.openGraph?.siteName,
       // images: ["/some-specific-page-image.jpg", ...previousImages],
+    },
+    twitter: {
+      title: post.title,
+      description: post.description,
     },
     keywords: [...(parent.keywords || [])], // ...(post.tags || [])],
   };
